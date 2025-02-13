5a930c9a03ed2237b1d3f99918fd73126ec915ed
jcoder2020 <jcoder2022@gmail.com>
2025-02-13 11:34:17 +0000

    First commit

---
+++ src/test/java/com/oauth2/login/oauth2_login_app/Oauth2LoginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2.login.oauth2_login_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2LoginAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
