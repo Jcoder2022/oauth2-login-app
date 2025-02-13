b4375432b33ba64171de0ca8f344d8e7f34b42fb
jcoder2020 <jcoder2022@gmail.com>
2025-02-13 12:44:31 +0000

    ADDED Repo

---
+++ src/main/java/com/oauth2/login/oauth2_login_app/repo/UserRepository.java
@@ -0,0 +1,7 @@
+package com.oauth2.login.oauth2_login_app.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class UserRepository {// implements JpaRepository<User,Long>{
+
+}
